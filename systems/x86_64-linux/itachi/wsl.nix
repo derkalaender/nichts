212cc1c791ca65f22e55abbae036d7f18103833d
derkalaender <git@derkalaender.de>
2025-05-11 07:31:18 +0200

    fix: usb forwarding

--- systems/x86_64-linux/itachi/wsl.nix
+++ systems/x86_64-linux/itachi/wsl.nix
@@ -1,11 +1,17 @@
-{ lib, ... }:
-with lib.nichts;
-{
-  wsl = enabled // {
-    defaultUser = "marvin";
-    wslConf = {
-      automount.enabled = true;
+{lib, ...}:
+with lib.nichts; {
+  wsl =
+    enabled
+    // {
+      defaultUser = "marvin";
+      wslConf = {
+        automount.enabled = true;
+      };
+      interop.includePath = false;
+      usbip = {
+        enable = true;
+        autoAttach = ["2-2"];
+        snippetIpAddress = "localhost";
+      };
     };
-    interop.includePath = false;
-  };
 }
