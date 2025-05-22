19b56ce662b0c16439a903711761309ae52c461d
derkalaender <git@derkalaender.de>
2025-05-22 14:47:07 +0200

    feat: fish as login shell

--- modules/nixos/shell/fish/default.nix
+++ modules/nixos/shell/fish/default.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }:
 with lib;
@@ -13,5 +14,17 @@ in {
   config = mkIf cfg.enable {
     # This is needed to get vendor completions
     programs.fish = enabled;
+
+    # fish as default launched from bash
+    # From: https://wiki.nixos.org/wiki/Fish
+    programs.bash = {
+      interactiveShellInit = ''
+        if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+        then
+          shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+          exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+        fi
+      '';
+    };
   };
 }
